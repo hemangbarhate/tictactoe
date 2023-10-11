a39ee3f8f926125d04cbf5ca3fab0102ff69b0cd
hemangbarhate <hemangbarhate@gmail.com>
2023-10-11 23:43:42 +0530

    setup

---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/widgets/custom_button.dart';
+import 'package:tictactoe/responsive/responsive.dart';
+import 'package:tictactoe/screens/join_room_screen.dart';
+import 'package:tictactoe/screens/create_room_screen.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  static String routeName = '/main-menu';
+  const MainMenuScreen({super.key});
+
+  void CreateRoom(BuildContext context) {
+    Navigator.pushNamed(context, CreateRoomScreen.routeName);
+  }
+
+  void JoinRoom(BuildContext context) {
+    Navigator.pushNamed(context, JoinRoomScreen.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Responsive(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButtom(onTap: () => CreateRoom(context), text: 'Create Room'),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButtom(onTap: () => JoinRoom(context), text: 'Join Room'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomButtom extends StatelessWidget {
+  final VoidCallback onTap;
+  final String text;
+  const CustomButtom({super.key, required this.onTap, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      decoration: const BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.teal,
+          blurRadius: 5,
+          spreadRadius: 0,
+        ),
+      ]),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+            minimumSize: Size(width, 50), backgroundColor: Colors.teal),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
